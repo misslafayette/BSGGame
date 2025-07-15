b3c3f80e5fa78db8509532be4c4518f19eafc189
Anita <andrasova42@gmail.com>
2025-07-15 21:47:36 +0200

    Add project files.

---
+++ BSGGame/GameLogic/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace BSGGame.GameLogic
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Loyalty { get; set; }
+        public List<string> _cards;
+
+
+    }
+}
